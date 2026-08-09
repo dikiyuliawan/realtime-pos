0bc7d7bb7fd69747d3ed57acd02bdc0b149b82b4
dikiyuliawan <dikiyuliawan@gmail.com>
2026-08-09 15:25:08 +0700

    setup supabase connection

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,38 @@
+import { cookies } from "next/headers";
+import { createServerClient as createSupabaseServerClient } from "@supabase/ssr";
+
+type createClientOptions = {
+  isAdmin?: boolean;
+};
+
+export async function createServerClient({
+  isAdmin = false,
+}: createClientOptions) {
+  const cookieStore = await cookies();
+  return createSupabaseServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    isAdmin
+      ? process.env.SUPABASE_SERVICE_ROLE_KEY!
+      : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll: () => cookieStore.getAll(),
+        setAll: (
+          cookiesToSet: Array<{
+            name: string;
+            value: string;
+            options?: Record<string, unknown>;
+          }>,
+        ) => {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch {
+            console.log("Error setting cookies");
+          }
+        },
+      },
+    },
+  );
+}
